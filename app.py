fe2773f4a468044a8ca0b56ee2362515d5db9ddf
Jayden <148350427+woojin9n@users.noreply.github.com>
2023-11-14 07:50:52 +0000

    updated app.py

--- app.py
+++ app.py
@@ -70,6 +70,8 @@ if user_input:
         loader = PyPDFDirectoryLoader(pdf_directory)
         pdf_data = loader.load_document(relevant_document_name)
 
+        print(pdf_data)
+
         # TODO: Process the PDF data as needed
         
         # Get response from GPT
