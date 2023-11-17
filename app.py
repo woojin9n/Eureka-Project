07429cc0ca414a8c55b5235b44e3c52843920578
Jayden <148350427+woojin9n@users.noreply.github.com>
2023-11-17 08:11:01 +0000

    updated

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ def index_documents_in_chroma(documents, client, db_name):
 # Load and index documents
 metadata_documents = load_and_process_documents(metadata_directory, '.json')
 pdf_documents = load_and_process_documents(pdf_directory, '.pdf')
-index_documents_in_chroma(metadata_documents, chroma_client, db_name='metadata_db')
-index_documents_in_chroma(pdf_documents, chroma_client, db_name='pdf_db')
+metadata_db = index_documents_in_chroma(metadata_documents, chroma_client, db_name='metadata_db')
+pdf_db = index_documents_in_chroma(pdf_documents, chroma_client, db_name='pdf_db')
 
 def get_gpt4_chat_response(prompt, context):
     # Function to get a response from GPT-4 using OpenAI API.
@@ -200,12 +200,12 @@ if user_input:
     context = ''
 
     # Search in metadata
-    metadata_results = chroma_client.search(query_embedding, k=1, db_name='metadata_db')
+    metadata_results = metadata_db.search(query_embedding, k=1, db_name='metadata_db')
     if metadata_results:
         context = metadata_results[0]['data']
     else:
         # Search in PDF documents if no results found in metadata
-        pdf_results = chroma_client.search(query_embedding, k=1, db_name='pdf_db')
+        pdf_results = pdf_db.search(query_embedding, k=1, db_name='pdf_db')
         if pdf_results:
             context = pdf_results[0]['data']
 
