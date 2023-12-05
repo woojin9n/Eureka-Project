243a52719b52ade71e3b4c572119e2ca37007406
lae04 <148328593+lae04@users.noreply.github.com>
2023-12-05 21:41:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
     submitButton.addEventListener("click", () => {
         const userQuery = userInput.value;
 
-        // You can make a POST request to your Python backend to handle the user query and retrieve a response.
-        // Here's a simplified example using Fetch API:
-        fetch("/api/get_response", {
-            method: "POST",
+        // Making an AJAX call to the app.py route with the user input
+        fetch('/api/get_response', {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ query: userQuery }),
+            body: JSON.stringify({ query: userQuery })
         })
-        .then((response) => response.json())
-        .then((data) => {
-            const gpt4Response = data.response;
-            responseContainer.innerHTML = `<p><strong>GPT-4 Response:</strong> ${gpt4Response}</p>`;
+        .then(response => response.json())
+        .then(data => {
+            // Displaying the response in the response-container
+            responseContainer.innerHTML = `<p>Here is the answer: ${data.response}</p>`;
+            responseContainer.style.display = 'block';
         })
-        .catch((error) => {
-            console.error("Error:", error);
+        .catch(error => {
+            console.error('Error:', error);
+            responseContainer.innerHTML = `<p>Error in fetching response</p>`;
         });
     });
+
+    // Event listener for the enter key in the input field
+    document.getElementById('user-input').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            submitButton.click();
+        }
+    });
 });
+
