40cac1c75d86a7d593579c9d8f6307f2d32ef848
Jayden <148350427+woojin9n@users.noreply.github.com>
2023-12-06 11:05:26 +0000

    updated

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     submitButton.addEventListener("click", () => {
         const userQuery = userInput.value;
-    })
-        // Making an AJAX call to the app.py route with the user input
         fetch("/api/get_response", {
             question : userQuery
-})
-.then((response) => response.json())
-.then((data) => {
-    const gpt4Response = data.response;
-    responseContainer.innerHTML = `<p><strong>GPT-4 Response:</strong> ${gpt4Response}</p>`;
-})
-.catch((error) => {
-    console.error("Error:", error);
-});
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            const gpt4Response = data.response;
+            responseContainer.innerHTML = `<p><strong>GPT-4 Response:</strong> ${gpt4Response}</p>`;
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 
+    })
+        // Making an AJAX call to the app.py route with the user input
+        
     // Event listener for the enter key in the input field
     document.getElementById('user-input').addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
