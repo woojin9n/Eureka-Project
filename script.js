95aa265f55e0a10ab9edc43212f0d139c895c991
Jayden <148350427+woojin9n@users.noreply.github.com>
2023-12-07 12:46:01 +0000

    setting route

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     submitButton.addEventListener("click", () => {
         const userQuery = userInput.value;
-        fetch("/(index)", {
+        fetch("/app", {
             headers: {
                 Accept: "application / json",
                 'Content-Type': 'application/json',
