98c945012991eddd1fa603e227eaa977b1bf95e9
Jayden <148350427+woojin9n@users.noreply.github.com>
2023-12-08 00:15:30 +0000

    fixed minor issues

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ for filename in os.listdir(pdf_directory):
             )
 
 # Set up Assistant API
-def get_response(user_input):
-    assistant = openai.beta.assistants.create(
+assistant = openai.beta.assistants.create(
     name="Tax Law chatbot",
     instructions="The main role of the tax law chatbot is to provide answers and solutions to questions requested by users, utilizing its tax law expertise and, now, the latest information in the OpenAI API documentation. Your job is to provide accurate tax law information in the context of the need, first by finding the appropriate content for the user's question in the JSON file data, and then by finding the appropriate content in the PDF file data related to the JSON file data. If a request is vague or incomplete, ask for more details to ensure an accurate and helpful response. Maintain a friendly and approachable tone while maintaining a professional demeanor. Treat users with respect and courtesy, and provide personalized answers when possible.",
     tools=[{"type": "retrieval"}],
@@ -38,8 +37,10 @@ def get_response(user_input):
     file_ids=[metafile.id,datafile.id]
     )
 
-    thread = openai.beta.threads.create()
+thread = openai.beta.threads.create()
 
+# Set up Run
+def get_response(user_input):
     message = openai.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
