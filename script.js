f9c6d12cf4781856a73746c1559494fed2dca8f6
lae04 <148328593+lae04@users.noreply.github.com>
2023-12-08 18:44:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     submitButton.addEventListener("click", () => {
         const userQuery = userInput.value;
-        fetch("/question", {
+        fetch("/", {
             //method: "POST",
             headers: {
                 Accept: "application/json",
