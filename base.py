1a802d15b62b65b7dba41ef229f47a4af6b47819
lae04 <148328593+lae04@users.noreply.github.com>
2023-12-13 16:13:36 +0900

    Update base.py

--- base.py
+++ base.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # 일반적인 라우트 방식입니다.
 @app.route('/a')
 def index():
-    return "Test"
-
+    return jsonify({"response": "aaa"})
+    
 if __name__ == '__main__':
     app.run(debug=True)
