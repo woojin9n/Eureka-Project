d33a042ec89075dfa3971e2983ca173849ab620e
lae04 <148328593+lae04@users.noreply.github.com>
2023-12-13 17:47:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,10 @@
-import os
-import openai
-import streamlit as st
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-# Set up OpenAI API Key
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# Set up directories for PDF files and metadata
-pdf_directory = "./data/"
-metadata_directory = "./metadata/"
-
-# Upload a file with an "assistants" purpose
-for filename in os.listdir(metadata_directory):
-    if filename.endswith(".json"): 
-        file_path = os.path.join(metadata_directory, filename)
-        with open(file_path, "rb") as file:
-            metafile = openai.files.create(
-                file=file,
-                purpose='assistants'
-            )
-
-for filename in os.listdir(pdf_directory):
-    if filename.endswith(".pdf"): 
-        file_path = os.path.join(pdf_directory, filename)
-        with open(file_path, "rb") as file:
-            datafile = openai.files.create(
-                file=file,
-                purpose='assistants'
-            )
-
-# Set up Assistant API
-assistant = openai.beta.assistants.create(
-    name="Tax Law chatbot",
-    instructions="The main role of the tax law chatbot is to provide answers and solutions to questions requested by users, utilizing its tax law expertise and, now, the latest information in the OpenAI API documentation. Your job is to provide accurate tax law information in the context of the need, first by finding the appropriate content for the user's question in the JSON file data, and then by finding the appropriate content in the PDF file data related to the JSON file data. If a request is vague or incomplete, ask for more details to ensure an accurate and helpful response. Maintain a friendly and approachable tone while maintaining a professional demeanor. Treat users with respect and courtesy, and provide personalized answers when possible.",
-    tools=[{"type": "retrieval"}],
-    model="gpt-4-1106-preview",
-    file_ids=[metafile.id,datafile.id]
-    )
-
-# Set up Thread and Run
-def get_response(user_input):
-    thread = openai.beta.threads.create()
-
-    message = openai.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=user_input
-    )
-
-    run = openai.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id
-    )
+# 일반적인 라우트 방식입니다.
+@app.route('/a')
+def index():
+    return jsonify({"response": "aaa"})
     
-    run = openai.beta.threads.runs.retrieve(
-    thread_id=thread.id,
-    run_id=run.id
-    )
-
-    messages = openai.beta.threads.messages.list(
-    thread_id=thread.id
-    )
-
-    return messages
-
-# Streamlit UI
-st.title('ChatGPT based on Tax Law')
-st.write('Type your question related to the Tax Law and get an answer.')
-
-# Input text box for user to ask questions
-user_input = st.text_input('Ask a question:')
-
-if user_input:
-    # TODO: You might want to preprocess or append context from your vector store
-    # to the user input before passing to the GPT model.
-    
-    reply = get_response(user_input)
-    st.write('Response:', reply)
+if __name__ == '__main__':
+    app.run(debug=True)
